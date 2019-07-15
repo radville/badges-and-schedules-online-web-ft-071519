03191da02dcd37b507ac3d649958d1b52b705482
Laura Radville <laura.radville@gmail.com>
2019-07-15 17:35:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,5 +2,14 @@ def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
+def batch_badge_creator(names)
+  new = []
+  names.each do |name|
+    new << "Hello, my name is #{name}."
+  end
+  new
+end
+
+def assign_rooms(speakers)
+  
 end
